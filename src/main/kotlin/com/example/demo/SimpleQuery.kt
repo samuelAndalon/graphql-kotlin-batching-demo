2c0897fef263de40eb305b84e732ca90464cda21
samvazquez <samvazquez@expedia.com>
2022-07-13 11:25:04 -0700

    feat: polish

--- src/main/kotlin/com/example/demo/SimpleQuery.kt
+++ src/main/kotlin/com/example/demo/SimpleQuery.kt
@@ -1,52 +1,23 @@
 package com.example.demo
 
-import com.expediagroup.graphql.dataloader.KotlinDataLoader
 import com.expediagroup.graphql.server.operations.Query
 import graphql.schema.DataFetchingEnvironment
-import org.dataloader.DataLoader
-import org.dataloader.DataLoaderFactory
-import org.dataloader.DataLoaderOptions
-import org.dataloader.stats.SimpleStatisticsCollector
 import org.springframework.stereotype.Component
-import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
-import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.toFlux
-import reactor.kotlin.core.publisher.toMono
 import java.util.concurrent.CompletableFuture
 
 data class User(val id: Int, val name: String, val lastName: String)
 
-@Repository
-class UserRepository {
-    private val users = listOf(
-        User(1, "John", "Doe"),
-        User(2, "Jane", "Doe")
+@Service
+class UserService {
+    private val users = mapOf(
+        1 to User(1, "John", "Doe"),
+        2 to User(2, "Jane", "Doe")
     )
 
-    fun getUser(id: Int): CompletableFuture<User?> =
-        CompletableFuture.completedFuture(
-            users.firstOrNull { user -> user.id == id }
-        )
-}
-
-@Service
-class UserService(private val userRepository: UserRepository) {
     fun getUsers(ids: Set<Int>): CompletableFuture<Map<Int, User?>> =
         CompletableFuture.completedFuture(
-            ids.associateWith { id -> userRepository.getUser(id).get() }
-        )
-}
-
-@Service
-class UserDataLoader(private val userService: UserService) : KotlinDataLoader<Int, User?> {
-    override val dataLoaderName: String = "UserDataLoader"
-    override fun getDataLoader(): DataLoader<Int, User?> =
-        DataLoaderFactory.newMappedDataLoader(
-            { ids ->
-                userService.getUsers(ids)
-            },
-            DataLoaderOptions.newOptions().setStatisticsCollector(::SimpleStatisticsCollector)
+            ids.associateWith { id -> users[id] }
         )
 }
 
@@ -63,24 +34,14 @@ class UserDataSource {
  * type Query {
  *     user(id: Int!): User
  * }
- *
  * type User {
  *     id: Int!
- *     lastName: String!
  *     name: String!
+ *     lastName: String!
  * }
  */
 @Component
-class SimpleQuery(
-    val userDataSource: UserDataSource
-) : Query {
+class SimpleQuery(val userDataSource: UserDataSource) : Query {
     fun user(id: Int, environment: DataFetchingEnvironment): CompletableFuture<User?> =
         userDataSource.getUser(id, environment)
-
-    fun foo(): Mono<List<String>> =
-        listOf("one".toMono(), "two".toMono()) // List<Mono<String>>
-            .toFlux()
-            .flatMap { it }
-            .collectList()
-
 }
